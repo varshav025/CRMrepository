5dccc9da69d10a62faa196801545e5abb9fe5c89
NANAND <NANAND@INBG-3XDNM33.commscope.com>
2024-10-14 14:16:00 +0530

    NAnditha first commit to Varsh Repo

---
+++ Architect/CRMArchitect_gitHub/src/test/java/nandithaAE/TestGitCommitMerge.java
@@ -0,0 +1,15 @@
+package nandithaAE;
+
+import org.testng.annotations.Test;
+
+public class TestGitCommitMerge  {
+	
+	@Test
+	public void test() {
+		
+		System.out.println("Nanditha created clone of Varsha repo and created a test class");
+	}
+
+	}
+
+
